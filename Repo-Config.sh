5d0533ad2a0f82b6b861e7110504ab57a7462f3e
Marzipanlord <WillClark05@protonmail.com>
2024-08-05 17:14:42 +0100

    Update Repo-Config.sh

--- Repo-Config.sh
+++ Repo-Config.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-clear
 cd
-echo -e "\nalias Fileman="bash ~/Git-Repos/Marzipanlord-Bash-Repo/Fileman.sh" >> ~/.bash_aliases"
-echo -e "\nalias cdback="bash ~/Git-Repos/Marzipanlord-Bash-Repo/CDBack.sh" >> ~/.bash_aliases"
+echo -e '\nalias Fileman="bash ~/Git-Repos/Marzipanlord-Bash-Repo/Fileman.sh" >> ~/.bash_aliases'
+echo -e '\nalias cdback="bash ~/Git-Repos/Marzipanlord-Bash-Repo/CDBack.sh" >> ~/.bash_aliases'
 clear
