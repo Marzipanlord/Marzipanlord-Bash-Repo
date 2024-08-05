43be21b8736466765a3ace1854a7313552671836
Marzipanlord <WillClark05@protonmail.com>
2024-08-05 16:54:15 +0100

    Update and rename First-run.sh to Repo-Config.sh

--- First-run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-clear
-cd
-echo -e "\nalias Fileman=bash ~/mpanlord-repo/Fileman.sh" >> .bash_aliases
-clear
---
+++ Repo-Config.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+clear
+cd
+echo -e "\nalias Fileman=bash ~/Git-Repos/Marzipanlord-Bash-Repo/Fileman.sh" >> .bash_aliases
+echo -e "\nalias cdback=bash ~/Git-Repos/Marzipanlord-Bash-Repo/CDBack.sh" >> .bash_aliases
+clear
